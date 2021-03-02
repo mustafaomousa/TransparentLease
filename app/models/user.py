9e9fa792b556fcb0101c46af0f55729ae9f8b7c6
Mustafa <mustafaomousa@icloud.com>
2021-03-02 12:38:34 -0600

    created the user table

--- app/models/user.py
+++ app/models/user.py
@@ -2,32 +2,40 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    profile_image = db.Column(db.String, nullable=True)
+    header = db.Column(db.String(150), nullable=True)
+    bio = db.Column(db.String, nullable=True)
+    broker = db.Column(db.Boolean)
+    dealer = db.Column(db.Boolean)
+    address = db.Column(db.String, nullable=True)
+    city = db.Column(db.String, nullable=True)
+    state = db.Column(db.String, nullable=True)
+    zip_code = db.Column(db.Integer, nullable=True)
+    created_at = db.Column(db.Date, nullable=True)
 
+    @property
+    def password(self):
+        return self.hashed_password
 
-  @property
-  def password(self):
-    return self.hashed_password
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
-
-
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
