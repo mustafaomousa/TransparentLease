c44fa0778b55b871144b3489d57a248ab19ada72
Mustafa <mustafaomousa@icloud.com>
2021-03-02 15:04:44 -0600

    created unprotected userid route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -40,12 +40,9 @@ function App() {
         <Route path="/sign-up" exact={true}>
           <SignUpForm authenticated={authenticated} setAuthenticated={setAuthenticated} />
         </Route>
-        <ProtectedRoute path="/users" exact={true} authenticated={authenticated}>
-          <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute path="/users/:userId" exact={true} authenticated={authenticated}>
+        <Route path="/:userId" exact={true}>
           <User />
-        </ProtectedRoute>
+        </Route>
         <Route path="/" exact={true} >
           <h1>My Home Page</h1>
         </Route>
