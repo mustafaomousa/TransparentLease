d5fbbf88bee745211e2936bffecabc93b7e859df
Mustafa <mustafaomousa@icloud.com>
2021-03-02 22:49:23 -0600

    added a not a member and already a member section

--- react-app/src/components/NavBarComponent/index.js
+++ react-app/src/components/NavBarComponent/index.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Menu, Dropdown, Icon } from "semantic-ui-react";
 
-const NavBarComponent = () => {
+import LogoutButton from "../auth/LogoutButton"
+
+const NavBarComponent = ({ setAuthenticated }) => {
     return (
-        <Menu>
-            <button>Test</button>
-        </Menu>
+        <div>
+            <Menu attached='top'>
+                <Menu.Item position='right' name='deals'>
+                    Deals
+                </Menu.Item>
+                <Menu.Item name="brokers">
+                    Brokers
+                </Menu.Item>
+                <Menu.Item name="calculator">
+                    Calculator
+                </Menu.Item>
+            </Menu >
+            <div>
+                Not a member? <Link to='/sign-up'>Click here!</Link>
+                Already a member? <Link to='/login'>Click here!</Link>
+                <LogoutButton setAuthenticated={setAuthenticated} />
+            </div>
+        </div>
+
     )
 };
 
