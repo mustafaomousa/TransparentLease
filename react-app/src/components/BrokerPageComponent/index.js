0ae62b7611d4a390d2fc204aea464f2c7bf58873
Mustafa <mustafaomousa@icloud.com>
2021-03-09 16:39:48 -0600

    updated broker page

--- react-app/src/components/BrokerPageComponent/index.js
+++ react-app/src/components/BrokerPageComponent/index.js
@@ -3,15 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { animateScroll } from "react-scroll";
 import { Send } from "grommet-icons";
-import { Avatar } from "@material-ui/core";
+import { Avatar, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Collapse } from "@material-ui/core";
 import { DataTable } from "grommet";
-import { FormControl, Input, Button, withStyles } from "@material-ui/core";
+import { FormControl, Input, Button, withStyles, IconButton, Paper } from "@material-ui/core";
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
 import "./brokerpage.css";
 import { getCurrentUser, getUserByUserName, getUserByUsername } from "../../store/user";
 import { createUserComment, getBrokerInformation } from "../../store/broker";
 import { createNotification } from "../../store/notifications";
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 const src = 'https://c0.klipartz.com/pngpicture/124/934/gratis-png-iconos-de-computadora-persona-avatar.png';
 
@@ -67,16 +69,66 @@ const columns = [
     },
 ];
 
+const Row = ({ deal }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <React.Fragment>
+
+
+            <TableRow>
+                <TableCell>
+                    <IconButton size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    <p>2021</p>
+                </TableCell>
+                <TableCell >
+                    <p>{deal.make.name}</p>
+                </TableCell>
+                <TableCell >
+                    <p>{deal.lease_info.trim.model.name}</p>
+                </TableCell>
+                <TableCell >
+                    <p>${deal.lease_info.msrp}</p>
+                </TableCell>
+                <TableCell >
+                    <p>{deal.lease_info.discount}</p>
+                </TableCell>
+                <TableCell >
+                    <p>{deal.lease_info.months}/{deal.lease_info.miles_yearly}</p>
+                </TableCell>
+                <TableCell >
+                    <p>{deal.lease_info.payment}</p>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <div>
+                            <h5>{deal.make.name}</h5>
+                        </div>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    )
+}
+
 
 const BrokerPageComponent = () => {
     const history = useHistory();
     const dispatch = useDispatch();
     const { brokerUsername } = useParams();
     const [newComment, setNewComment] = useState("");
+    const [open, setOpen] = useState(false)
     const broker = useSelector(state => state.broker.broker_information);
     const brokerDeals = useSelector(state => state.broker.broker_deals);
     const comments = useSelector(state => state.broker.broker_comments)
     const currentUser = useSelector(state => state.user)
+
 
     let data = []
     if (brokerDeals) {
@@ -138,12 +190,33 @@ const BrokerPageComponent = () => {
                     </div>
                 </div>
                 <div className="broker-table-container">
-                    <DataTable
+                    <TableContainer id="broker-page-deal-table" component={Paper}>
+                        <Table aria-label="collapsible table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell />
+                                    <TableCell>Year</TableCell>
+                                    <TableCell>Make</TableCell>
+                                    <TableCell>Model</TableCell>
+                                    <TableCell>MSRP</TableCell>
+                                    <TableCell>Discount</TableCell>
+                                    <TableCell>Term</TableCell>
+                                    <TableCell>Payment</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {brokerDeals && brokerDeals.map((deal, idx) => (
+                                    <Row deal={deal} />
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    {/* <DataTable
                         id="broker-table"
                         columns={columns}
                         data={data}
                         step={10}
-                    />
+                    /> */}
                 </div>
 
             </div>
