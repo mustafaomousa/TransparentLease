d5bd1d2a7d02c35227bd834f392825da1c281102
Mustafa Mousa <mustafaomousa@icloud.com>
2022-03-07 12:00:21 -0600

    update nav

--- react-app/src/components/NavBarComponent/index.js
+++ react-app/src/components/NavBarComponent/index.js
@@ -1,27 +1,70 @@
 import React, { useState } from "react";
-import { AppBar, Button } from "@material-ui/core"
-import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import SideBarComponent from "./SideBarComponent";
 import NotificationComponent from "../NotificationComponent";
 
-import './navbar.css';
+import "./navbar.css";
+import { useSelector } from "react-redux";
 
-const NavBarComponent = ({ user, setAuthenticated, setWelcomeOpen }) => {
-    const [sideHidden, setSideHidden] = useState(false)
+const Nav = () => {
+  const sessionUser = useSelector((state) => state.user);
 
-    return (
-        <div>
-            <AppBar id="navbar">
-                <Button onClick={(e) => setSideHidden(true)} ><MenuIcon id="sidebar-toggle" /></Button>
-                <h3>TransparentLease</h3>
-                <Button><NotificationsActiveIcon id="notifications-toggle" /></Button>
-            </AppBar>
-            <NotificationComponent />
-            <SideBarComponent user={user} sideHidden={sideHidden} setSideHidden={setSideHidden} setAuthenticated={setAuthenticated} setWelcomeOpen={setWelcomeOpen} />
-        </div>
-    )
+  return (
+    <AppBar>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            TransparentLease
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            TransparentLease
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+          {sessionUser ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="username">
+                <IconButton>
+                  <Avatar />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          ) : (
+            <Box sx={{ flexGrow: 0 }}>
+              <Button>Log in</Button>
+              <Button>Join</Button>
+            </Box>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
-export default NavBarComponent;
+export default Nav;
